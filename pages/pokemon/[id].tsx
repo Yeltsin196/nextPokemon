713a51e8a4801d970b44195be650f1c26f6a1ec7
Yeltsin <yramirez@shokworks.io>
2023-01-07 14:58:16 -0400

    fix pt2

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -109,15 +109,26 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths: pokemons151.map((id) => ({ params: { id } })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon: pokemon,
     },
+    revalidate: 10,
   };
 };
 export default PokemonPage;
